388e28da14aa13d6440dc20c2713c7812fe0b540
Adam Lotkowski <adm.lotkowski@gmail.com>
2021-11-22 03:42:17 +0100

    Dowload historical NPB's currency rates

--- Test1.py
+++ Test1.py
@@ -3,7 +3,7 @@ import json
 from urllib.error import URLError
 from urllib.request import urlopen
 from sqlite3 import Error
-from datetime import date, timedelta, datetime
+from datetime import date, timedelta
 
 
 def load_data(address_url):
@@ -48,25 +48,25 @@ def insert_data(conn, data, sql_insert_query, record1, record2, record3):
 
 def main():
 
-    database = 'nbpCurrencyRates.db'
-    conn = create_connection(database)
+    database = 'nbpCurrencyRates.db'                             # sqlite3 database file
+    conn = create_connection(database)                           # connecting with database
 
-    from_date = (2021, 9, 18)
+    from_date = (2021, 9, 18)                                    # data download start date
     print(from_date)
 
-    dif = date.today() - date(*from_date)
-    dif_in_days = dif.days
+    dif = date.today() - date(*from_date)                        # difference between start date to today date
+    dif_in_days = dif.days                                       # days difference in integer
     delta = 0
-    while delta < dif_in_days:
+    while delta < dif_in_days:                                   # creating url addresses for each date
         delta += 1
         date_to_load = date.today() - timedelta(days=delta)
         date_to_url = date_to_load.strftime("%Y-%m-%d")
         url = "https://api.nbp.pl/api/exchangerates/tables/a/{}/?format=json".format(date_to_url)
         print(url)
-        currency_rates = load_data(url)
-        if currency_rates is not None:
+        currency_rates = load_data(url)                          # download json file
+        if currency_rates is not None:                           # if download success then save data to database
 
-            date_to_insert = date_to_load.strftime("%Y_%m_%d")
+            date_to_insert = date_to_load.strftime("%Y_%m_%d")   # table name with correct date
             create_rates_table = """DROP TABLE IF EXISTS nbp"""+date_to_insert+""";
                                 CREATE TABLE nbp"""+date_to_insert+""" (
                                     Currency text,
@@ -84,6 +84,7 @@ def main():
                 print("Error! Cannot create the database connection.")
     conn.commit()
     conn.close()
+
 
 if __name__ == '__main__':
     main()
